ab20d709a6328b6fd64d744d71af355b87f11130
Fachri Akbar Rahmanda <fachriakbarrahmanda@gmail.com>
2025-07-23 17:59:41 +0700

    binary tree maze generation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
 import { usePathfinding } from "../hooks/usePathfinding";
+import { useTile } from "../hooks/useTile";
 import { MAZES } from "../utils/constants";
+import { resetGrid } from "../utils/resetGrid";
+import type { MazeType } from "../utils/types";
 import { Select } from "./Select";
+import { runMazeAlgorithm } from "../utils/runMazeAlgorithm";
+import { useSpeed } from "../hooks/useSpeed";
 
 export function Nav() {
-  const {maze} = usePathfinding();
+  const [isDisabled, setIsDisabled] = useState(false)
+  const {maze, setMaze, grid, setGrid, setIsGraphVisualized} = usePathfinding()
+  const {startTile, endTile} = useTile()
+  const {speed} = useSpeed()
+
+  const handleGenerateMaze = (maze: MazeType) => {
+    if (maze === 'NONE') {
+      setMaze(maze)
+      resetGrid({grid, startTile, endTile})
+      return
+    }
+
+    setMaze(maze)
+    setIsDisabled(true)
+    runMazeAlgorithm({
+      maze, grid, startTile, endTile, setIsDisabled, speed
+    })
+    const newGrid = grid.slice();
+    setGrid(newGrid)
+    setIsGraphVisualized(false)
+  }
 
   return (
     <div className="flex items-center justify-center min-h-[4.5rem] border-b shadow-gray-600 sm:px-5 px-0">
@@ -13,7 +39,7 @@ export function Nav() {
         </h1>
         <div className="flex sm:items-end items-center justify-start sm:justify-between sm:flex-row flex-col sm:space-y-0 space-y-3 sm:py-0 py-4 sm:space-x-4">
           <Select label="Maze" value={maze} options={MAZES} onChange={(e) => { 
-            //handle generatin maze 
+            handleGenerateMaze(e.target.value as MazeType)
           }}/>
         </div>
       </div>
